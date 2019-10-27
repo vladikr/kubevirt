37e2da5ecfac774950ecaa2628c9db90f135c3b4
Vladik Romanovsky <vromanso@redhat.com>
2019-10-26 23:18:22 -0400

    require guest time sync after a sucsessful migration
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

--- staging/src/kubevirt.io/client-go/api/v1/types.go
+++ staging/src/kubevirt.io/client-go/api/v1/types.go
@@ -355,6 +355,8 @@ type VirtualMachineInstanceMigrationState struct {
 	AbortStatus MigrationAbortStatus `json:"abortStatus,omitempty"`
 	// The VirtualMachineInstanceMigration object associated with this migration
 	MigrationUID types.UID `json:"migrationUid,omitempty"`
+	// Indicates that VMI Guest time sync is required
+	GuestTimeSyncRequired bool `json:"guestTimeSyncRequired,omitempty"`
 }
 
 // ---

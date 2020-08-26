8ef05cdfbf6f8d703bd2bce7ea54e853879d433b
Jed Lejosne <jed@redhat.com>
2020-08-26 17:44:41 -0400

    device-manager: misc post-rebase fixes
    Signed-off-by: Jed Lejosne <jed@redhat.com>

--- pkg/virt-api/webhooks/validating-webhook/admitters/vmi-create-admitter_test.go
+++ pkg/virt-api/webhooks/validating-webhook/admitters/vmi-create-admitter_test.go
@@ -1943,7 +1943,7 @@ var _ = Describe("Validating VMICreate Admitter", func() {
 			Expect(causes[0].Field).To(Equal("fake.HostDevices"))
 		})
 		It("should reject host devices that are not permitted in the hostdev config", func() {
-		        enableFeatureGate(virtconfig.HostDevicesGate)
+			enableFeatureGate(virtconfig.HostDevicesGate)
 			fakePermittedHostDevicesConfig := `
   pciDevices:
   - pciVendorSelector: "DEAD:BEEF"
@@ -1966,7 +1966,7 @@ var _ = Describe("Validating VMICreate Admitter", func() {
 			Expect(causes[0].Field).To(Equal("fake.HostDevices"))
 		})
 		It("should accept permitted host devices", func() {
-		        enableFeatureGate(virtconfig.HostDevicesGate)
+			enableFeatureGate(virtconfig.HostDevicesGate)
 			fakePermittedHostDevicesConfig := `
   pciDevices:
   - pciVendorSelector: "DEAD:BEEF"
